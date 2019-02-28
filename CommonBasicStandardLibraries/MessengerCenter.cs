ef5effc33b1caf8330ed8cfae804b4d7c9821dae
Andy <musictopia2@hotmail.com>
2019-02-28 10:02:39 -0700

    Change

--- CommonBasicStandardLibraries/MessengerCenter.cs
+++ CommonBasicStandardLibraries/MessengerCenter.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -9,7 +9,7 @@ namespace CommonBasicStandardLibraries.CrossPlatformMessenger
         public interface IMessagingCenter
         {
             void Send<TSender, TArgs>(TSender sender, string message, TArgs args) where TSender : class;
-            
+
             void Send<TSender>(TSender sender, string message) where TSender : class;
 
             void Subscribe<TSender, TArgs>(object subscriber, string message, Action<TSender, TArgs> callback, TSender source = null) where TSender : class;
@@ -112,7 +112,10 @@ namespace CommonBasicStandardLibraries.CrossPlatformMessenger
             void IMessagingCenter.Send<TSender, TArgs>(TSender sender, string message, TArgs args)
             {
                 if (sender == null)
+                {
                     throw new ArgumentNullException(nameof(sender));
+                }
+
                 InnerSend(message, typeof(TSender), typeof(TArgs), sender, args);
             }
 
@@ -124,7 +127,10 @@ namespace CommonBasicStandardLibraries.CrossPlatformMessenger
             void IMessagingCenter.Send<TSender>(TSender sender, string message)
             {
                 if (sender == null)
+                {
                     throw new ArgumentNullException(nameof(sender));
+                }
+
                 InnerSend(message, typeof(TSender), null, sender, null);
             }
 
@@ -136,9 +142,14 @@ namespace CommonBasicStandardLibraries.CrossPlatformMessenger
             void IMessagingCenter.Subscribe<TSender, TArgs>(object subscriber, string message, Action<TSender, TArgs> callback, TSender source)
             {
                 if (subscriber == null)
+                {
                     throw new ArgumentNullException(nameof(subscriber));
+                }
+
                 if (callback == null)
+                {
                     throw new ArgumentNullException(nameof(callback));
+                }
 
                 var target = callback.Target;
 
@@ -159,9 +170,14 @@ namespace CommonBasicStandardLibraries.CrossPlatformMessenger
             void IMessagingCenter.Subscribe<TSender>(object subscriber, string message, Action<TSender> callback, TSender source)
             {
                 if (subscriber == null)
+                {
                     throw new ArgumentNullException(nameof(subscriber));
+                }
+
                 if (callback == null)
+                {
                     throw new ArgumentNullException(nameof(callback));
+                }
 
                 var target = callback.Target;
 
@@ -197,13 +213,21 @@ namespace CommonBasicStandardLibraries.CrossPlatformMessenger
             void InnerSend(string message, Type senderType, Type argType, object sender, object args)
             {
                 if (message == null)
+                {
                     throw new ArgumentNullException(nameof(message));
+                }
+
                 var key = new Sender(message, senderType, argType);
                 if (!_subscriptions.ContainsKey(key))
+                {
                     return;
+                }
+
                 List<Subscription> subcriptions = _subscriptions[key];
                 if (subcriptions == null || !subcriptions.Any())
+                {
                     return; // should not be reachable
+                }
 
                 // ok so this code looks a bit funky but here is the gist of the problem. It is possible that in the course
                 // of executing the callbacks for this message someone will subscribe/unsubscribe from the same message in
@@ -223,7 +247,10 @@ namespace CommonBasicStandardLibraries.CrossPlatformMessenger
             void InnerSubscribe(object subscriber, string message, Type senderType, Type argType, object target, MethodInfo methodInfo, Filter filter)
             {
                 if (message == null)
+                {
                     throw new ArgumentNullException(nameof(message));
+                }
+
                 var key = new Sender(message, senderType, argType);
                 var value = new Subscription(subscriber, target, methodInfo, filter);
                 if (_subscriptions.ContainsKey(key))
@@ -240,16 +267,26 @@ namespace CommonBasicStandardLibraries.CrossPlatformMessenger
             void InnerUnsubscribe(string message, Type senderType, Type argType, object subscriber)
             {
                 if (subscriber == null)
+                {
                     throw new ArgumentNullException(nameof(subscriber));
+                }
+
                 if (message == null)
+                {
                     throw new ArgumentNullException(nameof(message));
+                }
 
                 var key = new Sender(message, senderType, argType);
                 if (!_subscriptions.ContainsKey(key))
+                {
                     return;
+                }
+
                 _subscriptions[key].RemoveAll(sub => sub.CanBeRemoved() || sub.Subscriber.Target == subscriber);
                 if (!_subscriptions[key].Any())
+                {
                     _subscriptions.Remove(key);
+                }
             }
 
             // This is a bit gross; it only exists to support the unit tests in PageTests
