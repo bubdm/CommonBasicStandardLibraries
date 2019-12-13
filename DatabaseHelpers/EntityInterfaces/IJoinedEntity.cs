c2808c3b21fd9d91a2bb6f74f2dedd6a2dda573f
Andy <musictopia2@hotmail.com>
2019-12-13 07:51:54 -0700

    Changed my mind about constraints

--- DatabaseHelpers/EntityInterfaces/IJoinedEntity.cs
+++ DatabaseHelpers/EntityInterfaces/IJoinedEntity.cs
@@ -2,13 +2,11 @@
 {
     public interface IJoinedEntity : ISimpleDapperEntity //hopefully i don't need more than 3 joined.  if so, rethink
     {
-        void AddRelationships<E>(E entity)
-            where E: class;
+        void AddRelationships<E>(E entity);
+            
     }
     public interface IJoin3Entity<D1, D2> : ISimpleDapperEntity
-        where D1: class
-        where D2: class
     {
-        void AddRelationships(D1? firstEntity, D2? secondEntity);
+        void AddRelationships(D1 firstEntity, D2 secondEntity);
     }
 }
