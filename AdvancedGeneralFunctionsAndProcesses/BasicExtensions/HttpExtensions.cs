97274a9080f01d11e5f12a9148bd47484a1dd791
Andy <musictopia2@hotmail.com>
2019-12-14 18:28:58 -0700

    Added more support to choose between URI or string.

--- AdvancedGeneralFunctionsAndProcesses/BasicExtensions/HttpExtensions.cs
+++ AdvancedGeneralFunctionsAndProcesses/BasicExtensions/HttpExtensions.cs
@@ -65,7 +65,23 @@ namespace CommonBasicStandardLibraries.AdvancedGeneralFunctionsAndProcesses.Basi
             StringContent content = new StringContent(thisStr);
             return await client.PutAsync(uri, content);
         }
-        
+        public static async Task<T> GetJsonAsync<T>(this HttpClient client, Uri uri, string errorMessage = "Failed to get async json data.  Rethink") //looks like delete is no problem.  not sure what patch is about anyways.
+        {
+
+            HttpResponseMessage response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode == false)
+                throw new BasicBlankException(errorMessage);
+            string res = await response.Content.ReadAsStringAsync();
+            response.Dispose();
+            try
+            {
+                return await js.DeserializeObjectAsync<T>(res);
+            }
+            catch (Exception)
+            {
+                throw new BasicBlankException("Failed to get the json.  Most likely, the page returned an error or the link was wrong");
+            }
+        }
         public static async Task<T> GetJsonAsync<T>(this HttpClient client, string uri, string errorMessage = "Failed to get async json data.  Rethink") //looks like delete is no problem.  not sure what patch is about anyways.
         {
             
