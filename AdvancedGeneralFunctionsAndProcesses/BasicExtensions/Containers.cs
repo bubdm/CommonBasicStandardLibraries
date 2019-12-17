943e1db52bcc8e52d657317d8f54118dec4d3bad
Andy <musictopia2@hotmail.com>
2019-12-17 11:09:14 -0700

    Register Non Window Classes

---
+++ AdvancedGeneralFunctionsAndProcesses/BasicExtensions/Containers.cs
@@ -0,0 +1,15 @@
+﻿using CommonBasicStandardLibraries.ContainerClasses;
+using CommonBasicStandardLibraries.MVVMHelpers;
+using CommonBasicStandardLibraries.MVVMHelpers.Interfaces;
+namespace CommonBasicStandardLibraries.AdvancedGeneralFunctionsAndProcesses.BasicExtensions
+{
+    public static class Containers
+    {
+        public static void RegisterNonWindows(this ContainerMain container)
+        {
+            container.RegisterInstance<ISimpleUI, NonWindowClass>();
+            container.RegisterInstance<IFocusOnFirst, NonWindowClass>();
+            //if there is anything else, i have it.
+        }
+    }
+}
