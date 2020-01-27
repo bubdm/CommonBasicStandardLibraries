9431f29d7a8fb71a92fbfede82cdd95d36da9a28
Andy <musictopia2@hotmail.com>
2020-01-26 19:04:47 -0700

    Added calendar window interface

---
+++ MVVMFramework/UIHelpers/ICalenderWindow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace CommonBasicStandardLibraries.MVVMFramework.UIHelpers
+{
+    public interface ICalenderWindow
+    {
+        void LoadCalender(DateTime date);
+        //for this first version, is needed.
+
+        event Func<DateTime, Task>? DateSelectedAsync;
+
+    }
+}
