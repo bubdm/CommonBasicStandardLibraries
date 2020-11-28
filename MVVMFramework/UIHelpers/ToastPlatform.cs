dce69fd4c662504b15688f2b972821249628cffc
Andy <musictopia2@hotmail.com>
2020-11-28 14:05:02 -0700

    Added class for toast helpers.  This will eventually be part of another library for .net 6.

---
+++ MVVMFramework/UIHelpers/ToastPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CommonBasicStandardLibraries.MVVMFramework.UIHelpers
+{
+    /// <summary>
+    /// this is a temporary class.  will eventually be part of another library for .net 6.  has here to prevent breaking changes until .net 6 comes out.
+    /// </summary>
+    public static class ToastPlatform
+    {
+        public static Action<string> ShowWarning { get; set; } = (message) =>
+        {
+            Console.WriteLine($"Warning { message}");
+        };
+
+        public static Action<string> ShowSuccess { get; set; } = (message) =>
+        {
+            Console.WriteLine($"Success { message}");
+        };
+
+        public static Action<string> ShowInfo { get; set; } = (message) =>
+        {
+            Console.WriteLine($"Info { message}");
+        };
+
+        //decided to go ahead and use the same name.  however, this means would have to have a using statement with abbrev to prevent compile errors.
+        public static Action<string> ShowError { get; set; } = (message) =>
+        {
+            Console.WriteLine($"Error { message}");
+        };
+
+    }
+}
