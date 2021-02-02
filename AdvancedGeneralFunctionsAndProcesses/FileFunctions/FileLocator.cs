f0337e7d02d5740e241da4de53ff2932fc4f2d40
Andy <musictopia2@hotmail.com>
2021-02-02 09:46:50 -0700

    New function which allows being able to access files in a repository and does not require being part of the project.

---
+++ AdvancedGeneralFunctionsAndProcesses/FileFunctions/FileLocator.cs
@@ -0,0 +1,17 @@
+﻿using aa = CommonBasicStandardLibraries.BasicDataSettingsAndProcesses.ApplicationPath;
+namespace CommonBasicStandardLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions
+{
+    public static class FileLocator
+    {
+        public static string MainLocation { get; set; } = "";
+        public static string GetLocation(string name)
+        {
+            string path = aa.GetApplicationPath();
+            int finds = path.IndexOf(MainLocation);
+            string modified = path.Substring(0, finds);
+            modified += MainLocation += @"\";
+            modified += name;
+            return modified;
+        }
+    }
+}
